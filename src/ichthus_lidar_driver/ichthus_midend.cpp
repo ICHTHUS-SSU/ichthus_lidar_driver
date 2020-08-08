fa76d6f8cd69cf1854bb0b06804c31ef4f2da1f4
Youngjoon Choi <youngjoon2@naver.com>
2020-08-08 17:58:43 +0900

    first commit

--- src/ichthus_lidar_driver/ichthus_midend.cpp
+++ src/ichthus_lidar_driver/ichthus_midend.cpp
@@ -26,7 +26,7 @@ namespace ichthus_lidar_mid
     while (ros::ok())
     {
       pcl::PointCloud<pcl::PointXYZINormal> out_cloud;
-      
+
       if (param_.mode_ == LIDAR_MODE::VLP16_MODE)
       {
         VLP16::add_VLP16_vector_msg_to_cloud(VLP16_buf_[buf_idx_], out_cloud, sin_rot_table_, cos_rot_table_, calibration_);
